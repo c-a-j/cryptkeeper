2c2bb02e6e881014d39f602d4f0ffb90b127f68b
Clint Jordan <clintaddison@gmail.com>
2026-03-08 18:52:20 -0600

    justfile updates

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -1,22 +1,32 @@
 function(ck_apply_compiler_options target)
   if (MSVC)
     target_compile_options(${target} PRIVATE /W4 /permissive-)
+  
     if (CK_WARNINGS_AS_ERRORS)
       target_compile_options(${target} PRIVATE /WX)
     endif()
+    
   else()
     target_compile_options(${target} PRIVATE
       -Wall -Wextra -Wpedantic
       -Wformat=2 -Wconversion -Wsign-conversion
       -Wshadow -Wnull-dereference -Wdouble-promotion
     )
+    
     if (CK_WARNINGS_AS_ERRORS)
       target_compile_options(${target} PRIVATE -Werror -Wno-error=unused-parameter)
     endif()
+    
     if (CK_ENABLE_SANITIZERS AND CMAKE_BUILD_TYPE STREQUAL "Debug")
-      target_compile_options(${target} PRIVATE -fsanitize=address,undefined -fno-omit-frame-pointer)
+      target_compile_options(${target} PRIVATE 
+        -fsanitize=address,undefined 
+        -fno-omit-frame-pointer
+        -fno-sanitize-recover=all
+        -g3
+      )
       target_link_options(${target} PRIVATE -fsanitize=address,undefined)
     endif()
+    
   endif()
 
   find_program(CLANG_TIDY_EXE NAMES clang-tidy)
--- justfile
+++ justfile
@@ -5,15 +5,19 @@ build_type := "Debug"
 binary := build_dir + "/src/ck"
 vault_dir := "~/.local/share/crypt-keeper"
 config_dir := "~/.config/crypt-keeper"
+asan_env := "ASAN_SYMBOLIZER_PATH=" + `which llvm-symbolizer` + " ASAN_OPTIONS=symbolize=1"
 
-configure tests="OFF":
-  cmake -S . -B {{ build_dir }} -DCK_ENABLE_SANITIZERS=OFF -DCMAKE_BUILD_TYPE={{ build_type }} -DBUILD_TESTING={{ tests }}
+configure sanitizers="OFF" tests="OFF":
+  cmake -S . -B {{ build_dir }} -DCMAKE_CXX_COMPILER=clang++ -DCK_ENABLE_SANITIZERS={{ sanitizers }} -DCMAKE_BUILD_TYPE={{ build_type }} -DBUILD_TESTING={{ tests }}
 
-build tests="OFF": (configure (tests))
+build tests="OFF": (configure "ON" (tests))
+  cmake --build {{ build_dir }}
+
+release: (configure ("OFF"))
   cmake --build {{ build_dir }}
 
 run *args: build
-  ./{{binary}} {{ args }}
+ {{ asan_env }} ./{{binary}} {{ args }}
 
 test filter="": (build ("ON"))
   cd {{ build_dir }} && ctest --output-on-failure {{ if filter != "" { "-R " + filter } else { "" }}} 
